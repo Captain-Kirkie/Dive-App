dd5b55deaa87c4de7e87883687e85214b10687e9
KirkHietpas <hietpaskirk@gmail.com>
2022-12-03 09:13:27 -0700

    wip: max depth

--- DiveApp/DiveApp/ContentView.swift
+++ DiveApp/DiveApp/ContentView.swift
@@ -8,10 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 1.4, contigency is 1.6. (relative % oxyagen)
+    @State private var maxPPG = 1.4;
+    @State private var contigencyPPG = 1.6;
+    @State private var Depth = 0; // measured in ATA
+    @State private var FG = 0; // fraction of oxygen, air is 21%
     @State private var oxygenPercent = 21.0;
     @State private var isEditing = false;
+    public var maxDepthFt = 0.0;
+    public var maxDepthM = 0.0;
     var body: some View {
-        Text("% oxygen \(oxygenPercent)")
+            
+        
+        Text("Max depth with % oxygen \(oxygenPercent) is \(maxDepthFt) ft, \(maxDepthM) M")
             .padding()
         Slider(
             value: $oxygenPercent,
